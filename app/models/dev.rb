214d9fef9df1dba36eebdc14209e9a08105b09fa
yoojin <yoojin9005@gmail.com>
2022-08-17 10:42:07 -0400

    Done with deliverables

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,4 +1,14 @@
 class Dev < ActiveRecord::Base
   has_many :freebies
   has_many :companies, through: :freebies
+
+  def received_one?(item_name)
+    self.freebies.pluck(:item_name).include?(item_name)
+  end
+
+  def give_away(dev, freebie)
+    devId = Dev.find_by(name: dev).id
+    belongItem = self.freebies.find_by("item_name = ?", freebie)
+    belongItem ? belongItem.update(dev_id: devId) : "no item be found"
+  end
 end
